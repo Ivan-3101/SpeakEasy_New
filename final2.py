396a1ecad3f11446d71a132d89d6ceebf2c62d6b
ivan <ivanad3101@gmail.com>
2024-03-25 21:24:37 +0530

    commented out modules import

--- final2.py
+++ final2.py
@@ -2,93 +2,13 @@ import streamlit as st
 import cv2
 import numpy as np
 import mediapipe as mp
-# from modules import mediapipe_detection, draw_styled_landmarks, extract_keypoints
+from modules import mediapipe_detection, draw_styled_landmarks, extract_keypoints
 from tensorflow.keras.models import load_model
 import os
 import pygame  # Import pygame for audio playback
-import gtts
 
 # Load the trained model
 model = load_model("action.h5")
-
-mp_holistic = mp.solutions.holistic  # Holistic model
-mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
-
-
-def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False  # Image is no longer writeable
-    results = model.process(image)  # Make prediction
-    image.flags.writeable = True  # Image is now writeable
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
-    return image, results
-
-
-def draw_styled_landmarks(image, results):
-    mp_drawing.draw_landmarks(
-        image,
-        results.face_landmarks,
-        mp_holistic.FACEMESH_CONTOURS,
-        mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
-        mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1),
-    )
-    mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_holistic.POSE_CONNECTIONS,
-        mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
-        mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2),
-    )
-    mp_drawing.draw_landmarks(
-        image,
-        results.left_hand_landmarks,
-        mp_holistic.HAND_CONNECTIONS,
-        mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-        mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2),
-    )
-    mp_drawing.draw_landmarks(
-        image,
-        results.right_hand_landmarks,
-        mp_holistic.HAND_CONNECTIONS,
-        mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
-        mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2),
-    )
-
-
-def extract_keypoints(results):
-    pose = (
-        np.array(
-            [
-                [res.x, res.y, res.z, res.visibility]
-                for res in results.pose_landmarks.landmark
-            ]
-        ).flatten()
-        if results.pose_landmarks
-        else np.zeros(33 * 4)
-    )
-    face = (
-        np.array(
-            [[res.x, res.y, res.z] for res in results.face_landmarks.landmark]
-        ).flatten()
-        if results.face_landmarks
-        else np.zeros(468 * 3)
-    )
-    lh = (
-        np.array(
-            [[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]
-        ).flatten()
-        if results.left_hand_landmarks
-        else np.zeros(21 * 3)
-    )
-    rh = (
-        np.array(
-            [[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]
-        ).flatten()
-        if results.right_hand_landmarks
-        else np.zeros(21 * 3)
-    )
-    return np.concatenate([pose, face, lh, rh])
-
 
 # Initialize variables
 actions = ["नमस्कार", "धन्यवाद", "होय"]
@@ -99,7 +19,6 @@ predicted_words = []
 # Initialize pygame mixer
 pygame.init()
 pygame.mixer.init()
-
 
 # Function to play audio
 def play_audio(filename):
@@ -140,9 +59,7 @@ st.sidebar.markdown(
 cap = cv2.VideoCapture(0)
 
 # Setting mediapipe model
-holistic = mp.solutions.holistic.Holistic(
-    min_detection_confidence=0.5, min_tracking_confidence=0.5
-)
+holistic = mp.solutions.holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 with holistic as holistic:
 
     # Create a container for the predicted word
